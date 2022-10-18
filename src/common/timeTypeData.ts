176ecd8b0a1eaea1d2cc86a141033ee48c314da7
Mioayaya <1530020617@qq.com>
2022-10-18 10:31:41 +0800

    优化if else if

---
+++ src/common/timeTypeData.ts
@@ -0,0 +1,31 @@
+import { utilsType } from "../type"
+
+export const TIMETYPEDATA:{
+  [keys:number]:utilsType.MtimeType
+} = {
+  5: 'morning',
+  6: 'morning',
+  7: 'morning',
+  8: 'morning',
+  9: 'morning',
+  10: 'morning',
+  11: 'noon',
+  12: 'noon',
+  13: 'noon',
+  14: 'noon',
+  15: 'afternoon',
+  16: 'afternoon',
+  17: 'afternoon',
+  18: 'afternoon',
+  19: 'evening',
+  20: 'evening',
+  21: 'evening',
+  22: 'evening',
+  23: 'night',
+  24: 'night',
+  0: 'night',
+  1: 'night',
+  2: 'beforeDawn',
+  3: 'beforeDawn',
+  4: 'beforeDawn',
+}
--- src/utils/methods/times.ts
+++ src/utils/methods/times.ts
@@ -1,21 +1,8 @@
+import { TIMETYPEDATA } from "../../common/timeTypeData";
 import { utilsType } from "../../type";
 
 export function getTimeType():utilsType.MtimeType {
-  let pType:utilsType.MtimeType = 'morning';
   const date = new Date();
   const pHours = date.getHours();
-  if(pHours>=5 && pHours <= 10) {
-    pType='morning';
-  }else if(pHours>=11 && pHours<=14) {
-    pType='noon';
-  }else if(pHours>=15 && pHours<=18) {
-    pType='afternoon';
-  }else if(pHours>=19 && pHours<=22) {
-    pType='evening';
-  }else if((pHours>=23 && pHours<=24) || (pHours>=0 && pHours<= 1)) {
-    pType='night';
-  }else if(pHours>=2 && pHours<=4) {
-    pType='beforeDawn';
-  }
-  return pType;
+  return TIMETYPEDATA[pHours];
 }
